f6d31802c00f2f1d09498b2455ecd34e961b1283
kohasuke <110949581+kobayashi1013@users.noreply.github.com>
2024-04-12 23:00:10 +0900

    push

--- Assets/Scripts/Scenes/InGame/Ball/BallMove.cs
+++ Assets/Scripts/Scenes/InGame/Ball/BallMove.cs
@@ -1,6 +1,7 @@
 using Scenes.InGame.Manager;
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 
 namespace Scenes.InGame.Ball
@@ -13,13 +14,29 @@ namespace Scenes.InGame.Ball
         [SerializeField, Tooltip("初速")]
         private float _power;
         private BallStatus _ballStatus;
+
+        private Vector2 _pastVelocity;
+
         void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
             _ballStatus = GetComponent<BallStatus>();
             _velocity = new Vector2(1,1).normalized;
             _rigidbody2D.AddForce(_velocity * _power, ForceMode2D.Impulse);
+
+            InGameManager.Instance.OnPause
+                .Subscribe(_ =>
+                {
+                    Pause();
+                }).AddTo(this);
+
+            InGameManager.Instance.OnRestart
+                .Subscribe(_ =>
+                {
+                    Restart();
+                }).AddTo(this);
         }
+
         //TODO:現在UpdateでずっとBallStatusを参照し続けています。イベント機能を使って、IsMovableの値が変更されたときだけ下の処理を実行するように変更してみましょう
         private void Update()
         {
@@ -40,6 +57,17 @@ namespace Scenes.InGame.Ball
                 var boundVelocity = transform.position - collision.gameObject.transform.position;
                 _rigidbody2D.AddForce(boundVelocity.normalized * _power, ForceMode2D.Impulse);
             }
+        }
+
+        private void Pause()
+        {
+            _pastVelocity = _rigidbody2D.velocity;
+            _rigidbody2D.velocity = Vector2.zero;
+        }
+
+        private void Restart()
+        {
+            _rigidbody2D.AddForce(_pastVelocity.normalized * _power, ForceMode2D.Impulse);
         }
     }
 }
--- Assets/Scripts/Scenes/InGame/InGameManager.cs
+++ Assets/Scripts/Scenes/InGame/InGameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using Scenes.InGame.Ball;
 using Scenes.InGame.Stick;
 using TMPro;
+using UniRx;
+using System;
 
 namespace Scenes.InGame.Manager
 {
@@ -17,6 +19,12 @@ namespace Scenes.InGame.Manager
         private int _blockSize = 0;//blockの数
         [SerializeField,Tooltip("スコアを表示するUI")]
         TextMeshProUGUI _socreText;
+
+        private Subject<Unit> Pause = new Subject<Unit>();
+        public IObservable<Unit> OnPause => Pause;
+        private Subject<Unit> Restart = new Subject<Unit>();
+        public IObservable<Unit> OnRestart => Restart;
+
         private void Awake()
         {
             if(Instance == null)
@@ -39,6 +47,16 @@ namespace Scenes.InGame.Manager
         {
             yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
             _ballSpawner.Spawn();
+        }
+
+        public void GamePause()
+        {
+            Pause.OnNext(default);
+        }
+
+        public void GameRestart()
+        {
+            Restart.OnNext(default);
         }
 
         public void GameOver()
